b50ed527fcbe8a098d7feccbbd535f77b2f764e5
OUREAlexandra <OUREAlexandra@users.noreply.github.com>
2020-12-26 21:17:12 +0100

    entity made

---
+++ src/Entity/Invoice.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvoiceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=InvoiceRepository::class)
+ */
+class Invoice
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="invoices")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $uuser;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Product::class, mappedBy="invoice")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUuser(): ?User
+    {
+        return $this->uuser;
+    }
+
+    public function setUuser(?User $uuser): self
+    {
+        $this->uuser = $uuser;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->addInvoice($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            $product->removeInvoice($this);
+        }
+
+        return $this;
+    }
+}
